a56e614ccc120ea7b930f8eec5ced012b0c0b83f
Dominic Baker <dominic.baker1@nhs.net>
2020-07-04 22:44:12 +0100

    added fail safe for user input

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ from game_logic import game_logic
 def main():
     
     user_input = input("Enter Rock, Paper or Scissors: ")
-    game_logic(user_input)
+    if (user_input != "Rock") or (user_input != "Paper") or (user_input != "Scissors"):
+        print("Please enter either 'Rock','Paper','Scissors'")
+    else:
+        game_logic(user_input)
 
 if __name__ == "__main__":
     main()
